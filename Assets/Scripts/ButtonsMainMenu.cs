bb5b45a691edf7a43c31dc705e1f7351c2d7dd00
reda0206 <cjones7@ltu.edu>
2025-05-02 16:12:49 -0400

    Finished Main Menu

--- Assets/Scripts/ButtonsMainMenu.cs
+++ Assets/Scripts/ButtonsMainMenu.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonsMainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void StartButton()
     {
-        
+        SceneManager.LoadScene("SampleScene");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitButton()
     {
-        
+       Application.Quit();
     }
 }
