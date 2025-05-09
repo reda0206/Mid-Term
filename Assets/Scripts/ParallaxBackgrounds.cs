ee077b5b8df73002e6d7153f7dcb6ce7b711bdb6
reda0206 <cjones7@ltu.edu>
2025-05-09 19:58:45 -0400

    Fixed Parallax

--- Assets/Scripts/ParallaxBackgrounds.cs
+++ Assets/Scripts/ParallaxBackgrounds.cs
@@ -4,30 +4,25 @@ using UnityEngine;
 
 public class ParallaxBackgrounds : MonoBehaviour
 {
-    private float startingPosX;
-    private float startingPosY;
+    private float startingPos;
     private float lengthOfSprite;
     public float AmountOfParallax;
     public Camera MainCamera;
     
     void Start()
     {
-        startingPosX = transform.position.x;
-        startingPosY = transform.position.y;
+        startingPos = transform.position.x;
         lengthOfSprite = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     
     void Update()
     {
-        Vector3 cameraPosition = MainCamera.transform.position;
-        float TempX = cameraPosition.x * (1 - AmountOfParallax);
-        float distanceX = cameraPosition.x * AmountOfParallax;
+        Vector3 Position = MainCamera.transform.position;
+        float Temp = Position.x * (1 - AmountOfParallax);
+        float distance = Position.x * AmountOfParallax;
 
-        float tempY = cameraPosition.y * (1 - AmountOfParallax);
-        float distanceY = cameraPosition.y * AmountOfParallax;
-
-        Vector3 newPosition = new Vector3(startingPosX + distanceX, startingPosY + distanceY, transform.position.y, transform.position.z);
+        Vector3 newPosition = new Vector3(startingPos + distance, transform.position.y, transform.position.z);
 
         transform.position = newPosition;
     }
